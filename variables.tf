cf9d0e37315bcfb6bcdb37e88f0e2592604a28fd
sarkyn7 <149303049+sarkyn7@users.noreply.github.com>
2023-11-28 14:52:04 +0530

    line1 add

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "The AWS region where the EC2 instance will be launched."
+  type        = string
+  default     = "ap-south-1"  # Set your preferred default region
+}
+
+variable "ami_id" {
+  description = "The ID of the AMI to use for the EC2 instance."
+  type        = string
+  default = "ami-02a2af70a66af6dfb"
+}
+
+variable "instance_type" {
+  type        = string
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  type        = string
+  default = "demu2"
+}
